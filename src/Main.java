6762f36ba6af6e94bb3583a2a123bff2c3b7d26f
Simeon <simeon.bobylev@gmail.com>
2014-05-20 15:13:26 +0200

    Création du GUI Login (2h30)
    Création de la fenêtre graphique pour la connexion sur le chat :
    - Login ou enregistrement d'un nouveau membre
    - Méthodes pour les clics sur les boutons implémentées
    - Design terminé à 99%

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import javax.swing.JFrame;
 
+import GUI.LoginGUI;
+
 
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 		JFrame client = new ICClient();
         client.setVisible(true);
         client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         client.pack(); 
+        */
+		
+		LoginGUI gui = new LoginGUI();
         
 	}
 
